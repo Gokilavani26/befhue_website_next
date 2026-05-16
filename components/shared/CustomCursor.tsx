4397ade646174bc9f3c84a9220c80362f6f86454
Gokilavani26 <gokilavani@swarmx.ai>
2026-05-16 14:37:36 +0530

    Cursor updated

--- components/shared/CustomCursor.tsx
+++ components/shared/CustomCursor.tsx
@@ -1,49 +1,69 @@
 "use client";
 
-import { useEffect, useState } from "react";
+import { useEffect, useRef } from "react";
 
 export default function CustomCursor() {
-  const [position, setPosition] = useState({
+  const cursorRef = useRef<HTMLDivElement>(null);
+
+  // Target mouse position
+  const mouse = useRef({
+    x: 0,
+    y: 0,
+  });
+
+  // Current animated position
+  const position = useRef({
     x: 0,
     y: 0,
   });
 
   useEffect(() => {
     const move = (e: MouseEvent) => {
-      setPosition({
-        x: e.clientX,
-        y: e.clientY,
-      });
+      mouse.current.x = e.clientX;
+      mouse.current.y = e.clientY;
     };
 
     window.addEventListener("mousemove", move);
 
+    let animationFrameId: number;
+
+    const animate = () => {
+      // Smooth interpolation
+      position.current.x +=
+        (mouse.current.x - position.current.x) * 0.12;
+
+      position.current.y +=
+        (mouse.current.y - position.current.y) * 0.12;
+
+      if (cursorRef.current) {''
+        cursorRef.current.style.transform = `
+          translate3d(
+            ${position.current.x - 1}px,
+            ${position.current.y - 1}px,
+            0
+          )
+        `;
+      }
+
+      animationFrameId = requestAnimationFrame(animate);
+    };
+
+    animate();
+
     return () => {
       window.removeEventListener("mousemove", move);
+      cancelAnimationFrame(animationFrameId);
     };
   }, []);
 
   return (
-    <>
-      {/* Floating Circle */}
-      <div
-        className="pointer-events-none fixed left-0 top-0 z-[9999] hidden md:block"
-        style={{
-          transform: `translate3d(${position.x - 11}px, ${position.y - 11}px, 0)`,
-          transition: "transform 0.02s linear",
-        }}
-      //  style={{
-      //         transform: `translate3d(${position.x - 6}px, ${position.y - 6}px, 0)`,
-      //     }}
-      >
-        <div className="relative flex h-10 w-10 items-center justify-center">
-          {/* Outer Ring */}
-          {/* <div className="absolute inset-0 rounded-full border border-[#ff002b]" /> */}
-
-          {/* Inner Dot */}
-          <div className="h-9.5 w-9.5 rounded-full bg-[#ff002b]" />
-        </div>
+    <div
+      ref={cursorRef}
+      className="pointer-events-none fixed left-0 top-0 z-[9999] hidden md:block"
+    >
+      <div className="relative flex h-10 w-10 items-center justify-center">
+        <div className="h-9 w-9 rounded-full bg-[#ff002b]" />
       </div>
-    </>
+    </div>
   );
 }
